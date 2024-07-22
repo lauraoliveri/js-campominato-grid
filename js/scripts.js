75c77058a06c33069eec092cb712605f6b72034c
lauraoliveri <lauraoliveri2003@gmail.com>
2024-07-22 18:28:00 +0200

    Add play button in js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+// crea bottone 
+const PlayButton = document.getElementById('play_btn');
+
+// quando lìutente clicca il bottone allora
+
+PlayButton.addEventListener('click', function() {
+
+
+    
+})
