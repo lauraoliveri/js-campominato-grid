e80503c1aee417794923da7ee279bdef5c617a83
lauraoliveri <lauraoliveri2003@gmail.com>
2024-07-23 09:42:12 +0200

    Edit js

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,24 @@ const PlayButton = document.getElementById('play_btn');
 
 PlayButton.addEventListener('click', function() {
 
+    // contenitore della griglia
+    const Grid = document.getElementById('grid_container');
+    console.log('Grid', Grid, typeof Grid);
 
-    
+    for(let i = 0; i < 100; i++) {
+
+        const NewCell = document.createElement('div');
+        NewCell.classList.add("grid_container_cells");
+        console.log('Nuova Cella', NewCell, typeof NewCell);
+        NewCell.innerHTML = '<h3>' + ( i + 1 ) + '</h3>';
+
+        // aggiungo le nuova celle al contenitore della griglia
+        Grid.append(NewCell);
+    }
+
+
 })
+ 
+
+
+
